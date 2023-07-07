ae34a049d88b664a220c5ce48c214f012afc16fb
Krzysztof Sinica <krzysztof@sinica.pl>
2023-07-07 16:21:09 +0200

    WIP

--- int_map.go
+++ int_map.go
@@ -16,6 +16,7 @@ type intMap[T any] struct {
 	m map[string]intMapEntry[T]
 }
 
+// set sets the value for a key.
 func (m *intMap[T]) set(key *big.Int, value T) {
 	if m.m == nil {
 		m.m = make(map[string]intMapEntry[T])
@@ -27,6 +28,8 @@ func (m *intMap[T]) set(key *big.Int, value T) {
 	runtime.KeepAlive(key)
 }
 
+// get returns the value stored in the map for a key, or zero value if no value
+// is present. The ok result indicates whether value was found in the map.
 func (m *intMap[T]) get(key *big.Int) (value T, ok bool) {
 	if m.m != nil {
 		e, ok := m.m[string(unsafeToBytes(key))]
@@ -36,6 +39,8 @@ func (m *intMap[T]) get(key *big.Int) (value T, ok bool) {
 	return
 }
 
+// contains returns true if the map contains some value associated with
+// a given key.
 func (m *intMap[T]) contains(key *big.Int) bool {
 	_, ok := m.get(key)
 	return ok
@@ -49,6 +54,8 @@ func (m *intMap[T]) first() (ret T) {
 	return
 }
 
+// forEach calls f sequentially for each key and value present in the map.
+// If f returns false, it stops the iteration.
 func (m *intMap[T]) forEach(f func(key *big.Int, value T) bool) {
 	for _, v := range m.m {
 		if !f(v.key, v.value) {
